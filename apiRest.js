9692adfdfca5825fc3da3f084e18d3304713fcc9
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-08-28 00:26:34 -0300

    implementando metodo post

--- apiRest.js
+++ apiRest.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 let meusProdutos = fs.readFileSync('produtos.json')
 let produtos = JSON.parse(meusProdutos)
@@ -10,9 +14,27 @@ app.get('/', (req,res,next) => {
     next(error => console.error(error))
 })
 
+// rota pra listar os produtos
 app.get('/produtos', (req, res, next) => {
     res.json(produtos)
 })
+
+// rota pra cadastrar produtos
+app.post('/novosProdutos', (req, res, next) => {
+    const novoProduto = req.body;
+
+    if(Array.isArray(novoProduto)){
+        for(item of produtos){
+            produtos.push(item)
+        }
+    }else{
+            produtos.push(novoProduto)
+        }
+        let jsonList = JSON.stringify(produtos)
+        fs.writeFile('produtos.json', jsonList, 'utf8', () => {})
+        res.send('Produto cadastrado com sucesso')
+    }
+);
 
 app.listen(3000, () => {
     console.log("rodando");
