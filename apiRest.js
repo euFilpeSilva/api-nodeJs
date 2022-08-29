f45e2b0cafd522e776736e355449d82f3046a3c1
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-08-29 09:32:08 -0300

    atualizando coments

--- apiRest.js
+++ apiRest.js
@@ -1,7 +1,15 @@
 const express = require('express')
+
+// bodyParser serve para trabalhar com os dados vindo do seu cliente.
+//  Quando o cliente manda dados via form payload, esse pacote ele formata
+//   e transforma os dados para o formato de objeto javascript e joga tudo 
+//   isso em um objeto dentro do objeto de requisição (req): req. body.
 const bodyParser = require('body-parser')
+
+// fs é utilizado pra acessar o caminho raiz dos meus diretorios
 const fs = require('fs')
 const app = express()
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -33,9 +41,8 @@ app.get('/produtos/:id', (req, res) => {
 });
 
 // rota pra cadastrar produtos
-app.post('/novosProdutos', (req, res, next) => {
+app.post('/novosProdutos', (req, res) => {
     const novoProduto = req.body;
-
     if(Array.isArray(novoProduto)){
         for(item of produtos){
             produtos.push(item)
@@ -46,20 +53,39 @@ app.post('/novosProdutos', (req, res, next) => {
         let jsonList = JSON.stringify(produtos)
         fs.writeFile('produtos.json', jsonList, 'utf8', () => {})
         res.send('Produto cadastrado com sucesso')
-    }
+
+// const {nome, marca, preco} = req.body;
+// const produto = {
+//     id: uuid(),
+//     nome,
+//     marca,
+//     preco
+// }
+
+// produtos.push(produto)
+// return res.status(201).json(produto)
+
+     }
 );
 
-app.delete('/produtoo/:id',(req,res)=> {
+// rota pra deletar produtos
+app.delete('/produto/:id',(req,res)=> {
     const id = req.params.id
-    for(let i=0; i < produtos.length; i++) {
-        if(produtos[i].id == id){
-            produtos.splice(i,1)
-            res.send('Dados apagados com sucesso!')
-            return
-    }
-  }
-  res.status(404).send('Produto não encontrado!')
+
+        for(let i=0; i < produtos.length; i++) {
+                if(produtos[i].id == id){
+                        produtos.splice(i,1)
+                        res.send('Dados apagados com sucesso!')
+                        return
+                }
+              }
+            res.status(404).send('Produto não encontrado!')
 });
+
+// renderizar em uma porta localhost
+app.put('/atualizar/id', (req,res)=> {
+    const {id} = req.params; 
+})
 
 
 app.listen(3000, () => {
