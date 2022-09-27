3575aed9aeadde25d15a45c3211c05a13ccf817e
devbispo <93449428+devbispo@users.noreply.github.com>
2022-09-27 20:42:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./src/swagger.json')
 const app = express()
 
 const bodyParser = require('body-parser');
@@ -6,6 +8,7 @@ const routes = require('./src/routes/productRoutes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes)
 
 module.exports = app;
