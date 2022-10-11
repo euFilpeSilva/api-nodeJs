54b244c7fafea9b0773dc57cc3eec765090cb6fb
FILIPE SOUSA DA SILVA <79103757+euFilpeSilva@users.noreply.github.com>
2022-10-10 23:11:32 -0300

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -8,15 +8,15 @@ const routes = express.Router();
 routes.get("/produtos", productControllers.listar);
 
 // Consultar um unico produto
-routes.get("/produtosPorId/:id", productControllers.listarPorId);
+routes.get("/produto/:id", productControllers.listarPorId);
 
 // Cadastrar produtos
 routes.post("/criarProduto", productControllers.criar);
 
 // Deletar produtos
-routes.delete("/deleteProduto/:id", productControllers.remover);
+routes.delete("/produto/:id", productControllers.remover);
 
 // Atualizar produto
-routes.put("/atualizaProduto/:id", productControllers.atualizar);
+routes.put("produto/:id", productControllers.atualizar);
 
 module.exports = routes;
